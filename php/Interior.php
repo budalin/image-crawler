d569b2486bbbd93da526267c005b7a2f78a0ad37
htun <htunlinnaung@appvantage.sg>
2015-07-17 09:59:27 +0630

    adding trims function to interior class

--- php/Interior.php
+++ php/Interior.php
@@ -3,6 +3,7 @@
 /**
  * Download Interior image for specific Class
  * Date - 14/07/2015
+ * Author - htunlinnaung
  * Version - 0.0.1
  */
 
@@ -15,48 +16,191 @@ class Interior
 	public $colors;
 	public $our_colors;
 	public $json;
+
 	
-	function get($property)
+	/*public function __construct()
 	{
-		
+		$Toolkit_Dir = '../toolkit/';
+		include $Toolkit_Dir . 'JPEG.php';                     
+	}*/
+
+	/*
+	* Tracing purpose only
+	*/
+	static function dump($val, $type = false)
+	{
+		if(is_string($val)):
+			echo $val.'<br/>';
+		elseif(is_array($val)):
+			echo '<pre>';print_r($val);echo '</pre><br/>';
+		elseif($type == true):
+			var_dump($val);
+		endif;
+	}
+	
+	/**
+	 * Getter Method
+	 */
+	function get($property)
+	{		
 		return $this->property;	
 	}
 
+	/**
+	 * Setter Method
+	 */
 	function set($property,$value)
 	{
-		$this->property = $value;
-		
+		$this->property = $value;		
 	}
 
-	// function interior_image($this->image_url, $this->base_folder, $this->index, $this->colors, $this->our_colors)
-	// {
+	/**
+	 * $metada(string) - Key-Value pair from the form input
+	 * $image_url(string) - Image URL from the form input
+	 * $foldername(string) - Folder name from the form input
+	 * $colors(array) - Input colors from the color picker
+	 */
+	function interior_image($metadata, $image_url, $foldername, $colors)
+	{
+		$available_codes = Self::__available('interior_colors.json');
+		$interior_trims = array('cIQC3','5MQC3','5uQC3');
+		$available_trims = Self::__available('interior_trim_colors.json');
+		$interior_views = array('IMGT=A4&POV=BI4','IMGT=A4&POV=BI3','IMGT=A4&POV=BI2');
+		$available_views = array('IMGT=A27&POV=BI1','IMGT=A27&POV=BI2','IMGT=A27&POV=BI3','IMGT=A27&POV=BI4','IMGT=A4&POV=BI4','IMGT=A4&POV=BI3','IMGT=A4&POV=BI2','IMGT=A4&POV=BI1');
+		
+		// Self::dump($available_trims);exit;
+		
+		if(is_array($colors)):
 
-	// }
+			#loop for color picker
+			for($i = 0; $i<count($colors); $i++):
+				$codecolor = explode('|', $colors[$i]);
+				$filenames[] = $codecolor[0];
+				$urlcodes[] = $codecolor[1];
 
-	function available_colors($json)
+			endfor;
+		endif;	
+		
+		for ($i=0; $i < count($urlcodes); $i++): 
+			
+			for ($j=0; $j < count($available_codes); $j++):
+
+				$urlcode = $available_codes[$j];
+
+				if(strpos($image_url, $urlcode)):
+
+                	$new_image_url = str_replace($urlcode, $urlcodes[$i], $image_url);
+					// echo 'Found color in ->'.$i.' '.$j;
+                	// echo $new_image_url;
+					
+				endif;
+
+			endfor;
+
+			# loop for view
+			for ($j=0; $j < count($interior_views); $j++) : 
+
+				
+				for ($k=0; $k < count($available_views) ; $k++):
+
+	                if (strpos($new_image_url, $available_views[$k])):
+                    
+	                    $new_view = str_replace("$available_views[$k]", "$interior_views[$j]", $new_image_url);
+	                	$gg[] = $new_view;
+						switch($j){
+							case 0:
+				                $filename = $filenames[$i].'_front_back.jpg';
+				                break;
+				            case 1:
+				                $filename = $filenames[$i].'_back.jpg';
+				                break;
+				            case 2:
+				                $filename = $filenames[$i].'_front.jpg';
+				                break;
+
+						}	
+						// Self::write_into_file($metadata, $foldername, $filename, $new_view);
+						// echo 'Found view in ->'.$i.' '.$k;
+						// Self::dump($new_image_url);
+
+					endif;
+
+				endfor;
+					
+			endfor;
+
+		endfor;
+		
+		Self::dump($gg);exit;
+
+	}
+
+	static function trims($metadata,$image_urls)
+	{
+
+	}
+
+	static function write_into_file($metadata, $foldername, $filename, $image_url)
+	{
+    	$base_folder = "images/";
+		$toroot = $_SERVER['DOCUMENT_ROOT'] .$_SERVER['REQUEST_URI'];
+        $toroot = substr($toroot, 0, -9);
+		$curl_info = Self::get_info_remote_file($image_url);
+
+		if($curl_info == 'image/jpeg'):
+    		$folderpath = $toroot.$base_folder.$foldername;
+				$content = file_get_contents($image_url);
+				if(!file_exists($folderpath)):
+	  				if(!mkdir($folderpath, 0777, true)):
+	  					die('Folder create failed ');
+	  				endif;
+				endif;
+
+				$file = $folderpath.'/'.$filename;
+				$filePut = file_put_contents($file, $content);
+	        if($filePut):
+	            chmod($file, 0777);
+	            $jpeg_header_data = get_jpeg_header_data( $file );
+	            $new_header_data = put_jpeg_Comment( $jpeg_header_data, $metadata );
+	            put_jpeg_header_data($file,$file,$new_header_data);
+	        else:
+	            die('File put failed');
+	        endif;
+
+    	
+    	endif;
+
+	}
+
+	static function __available($json)
 	{
 		$jsonFile = file_get_contents('../data/'.$json);
     	$interiorColors = json_decode($jsonFile, true);
-    	$url_codes = array();
+    	
+    	
 
 		foreach ($interiorColors as $color_codes) {
-			$url_codes[] = $color_codes['url_code'];
-		}
-
-		return $url_codes;
-	}
-
-	function change_name($index)
-	{
-		switch ($index) {
-			case 'value':
-				# code...
-				break;
+			$available[] = $color_codes['url_code'];
 			
-			default:
-				# code...
-				break;
 		}
+		
+		return $available;
 	}
+
+	static function get_info_remote_file($url)
+	{
+	    $ch = curl_init($url);
+	    curl_setopt($ch, CURLOPT_NOBODY, 1);
+	    curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	    curl_exec($ch);
+
+	    # get the content type
+	    $info = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+	    
+	    return $info;
+	}
+
+
 }
 
